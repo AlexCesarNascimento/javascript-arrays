71cb5869930d13ba0264edbc53c355144698df33
Alex Cesar Nascimento <alexcesar.n@gmail.com>
2022-04-30 11:27:19 -0300

    feat(aula03): concatenando arrays

---
+++ aula03 - Concatenando Arrays/02_(...rest)_concatenando_com_spread_operator.js
@@ -0,0 +1,9 @@
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+// Operador de espalhamento (spread) -> ...rest
+const array3 = [...array1, ...array2];
+console.log(array3);    // [ 1, 2, 3, 4, 5, 6 ]
+
+const array4 =  [...array1, ...array2, ...[7, 8, 9], "Alex"];
+console.log(array4);    // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, "Alex" ]
